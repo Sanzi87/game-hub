db2509851670375fd96d4580188664e980681935
Alexander Blomberg <info@dalasoftware.se>
2023-05-06 11:34:16 +0200

    Refactor- create lookup hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import platforms from '../data/platforms';
-import APIClient, { FetchResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
 
@@ -17,7 +17,7 @@ const usePlatforms = () =>
         queryKey: ['platforms'],
         queryFn: apiClient.getAll,
         staleTime: 24 * 60 * 60 * 1000, //24h,
-        //initialData: { count: platforms.length, result: platforms } //error with .length?
+        initialData: platforms
 })
 
 export default usePlatforms;
